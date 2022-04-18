dfa276901c8bb3f1c919a0988710e8eaa03c39d4
fed1k <allamurotovcoder@mail.ru>
2022-04-18 21:17:53 +0500

    Fix js linters

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <App />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
