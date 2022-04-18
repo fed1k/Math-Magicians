dbb867c1b7492d253f3e58f404ce2005b431b5ea
fed1k <allamurotovcoder@mail.ru>
2022-04-18 21:08:19 +0500

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit 
+        <p>
+          Edit
           <code>src/App.js</code>
           and save to reload.
         </p>
