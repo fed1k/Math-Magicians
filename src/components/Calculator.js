97012b80afec6d316c1e46260300b24d708181a4
fed1k <allamurotovcoder@mail.ru>
2022-04-21 16:30:39 +0500

    Include calculate and operate file in logic folder

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,19 +4,21 @@ let count = 1;
 class Design extends React.Component {
   constructor(props) {
     super(props);
+    this.data = ['AC', '+/-', '%', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='],
     this.state = {
-      data: ['AC', '+/-', '%', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='],
-      classname: 'a',
+      presenter: 0,
     };
+  }
+  handler(e){
+    this.setState({presenter: e})
   }
 
   render() {
-    const el = this.state;
     return (
       <div className="main-container">
-        <div className="console">0</div>
-        {el.data.map((i) => {
-          return <span className={el.classname + (count += 1)} key={(count += 1)}>{i}</span>
+        <div className="console">{this.state.presenter}</div>
+        {this.data.map((i) => {
+          return <button key={(count += 1)}>{i}</button>
         })}
       </div>
     );
