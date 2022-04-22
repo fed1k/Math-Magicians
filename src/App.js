0e3057ba2284e1e1e929c406a9e43b85ce7d1b08
fed1k <allamurotovcoder@mail.ru>
2022-04-22 21:30:24 +0500

    Change divide chraracter with the correct one

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Design from './components/Calculator';
 
 function App() {
-  const buttonNames = ['AC', '+/-', '%', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+  const buttonNames = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
   
   return (
     <div className="App">
